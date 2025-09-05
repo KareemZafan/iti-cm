b6ef20509e5024330b514b14e22b953c2b8d76b8
KareemZafan <kareemm.zafan@gmail.com>
2025-09-05 15:10:02 +0300

    Adding sqrt function

--- src/main.c
+++ src/main.c
@@ -1,8 +1,14 @@
 #include <stdio.h>
 
+#include <math.h>
 
+double getSquareRoot(double a){
+ return sqrt(a); 
+}
 int main(void){
 
 printf("Hello World!"); 
+
+printf("Square root of 100 = %0.3f",getSquareRoot(100)); //10.000
 
 return 0; }
